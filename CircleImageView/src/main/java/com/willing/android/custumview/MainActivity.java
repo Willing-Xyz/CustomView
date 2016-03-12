ea4c7c38b0c43124a73bbd0699407614066bb16f
Willing-Xyz <971782067@qq.com>
2016-03-12 17:35:19 +0800

    圆形图片View

---
+++ CircleImageView/src/main/java/com/willing/android/custumview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.willing.android.custumview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Willing on 2016/3/12.
+ */
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout_main);
+    }
+}
